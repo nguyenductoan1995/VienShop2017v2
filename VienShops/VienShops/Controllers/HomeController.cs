18dd95c93a6a47850fad06f3fcf62712c2ca2226
nguyenductoan1995 <nguyenductoann@gmail.com>
2017-10-23 01:34:53 +0700

    Toàn

---
+++ VienShops/VienShops/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VienShops.Models;
+// Khai báo thư viện phân trang
+using PagedList;
+using PagedList.Mvc;
+namespace VienShops.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBVienShopsDataContext Db = new DBVienShopsDataContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        // sản phẩm mới 
+        public ActionResult NewProduct()
+        {
+            var newProduct = Db.SANPHAMs.OrderBy(n => n.MASP).Take(4).ToList();
+            return PartialView(newProduct);
+        }
+        public ActionResult FeaturedProduct()
+        {
+            var featuredProduct = Db.SANPHAMs.OrderBy(n => n.CHATLIEU).Take(4).ToList();
+            return PartialView(featuredProduct);
+        }
+        public ActionResult Products()
+        {
+            var products = Db.LOAISANPHAMs.ToList();
+            return PartialView(products);
+        }
+        // Show products list
+        public ActionResult ShowProductList(int? page, int id)
+        {
+            int pageSize = 9;
+            // Nếu trang bé hơn 9 trả về 1 trang
+            int pageNumber = (page ?? 1);
+            //var tenLoai = Db.LOAISANPHAMs.SingleOrDefault(n => n.MALOAISP == id);
+            var productList = Db.SANPHAMs.Where(n => n.MALOAISP == id).ToList().OrderBy(n => n.TENSP).ToPagedList(pageNumber, pageSize);
+            // Gán mã, khi load form lưu id
+            ViewBag.ID = id;
+            return View(productList);
+        }
+        // Show products list giao diện 2
+        public ActionResult ShowProductListTheme2(int? page,int id)
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var productList = Db.SANPHAMs.Where(n => n.MALOAISP == id).ToList().OrderBy(n => n.TENSP).ToPagedList(pageNumber, pageSize);
+            ViewBag.ID = id;
+            return View(productList);
+        }
+        // Menu Trái của Loại sản phẩm 
+        public PartialViewResult MenuLeft()
+        {
+            return PartialView();
+        }
+        // Liên hệ 
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        // Chi tiết sản phẩm 
+        public ActionResult Detail(int id)
+        {
+            SANPHAM sp = Db.SANPHAMs.SingleOrDefault(n => n.MASP == id);
+            if(sp == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+			// Truyền vào model là sản phẩm
+			return View(sp);
+        }
+		// Sản phẩm theo loại - Sản phẩm liên quan khi xem trang chi tiết - Sản phẩm theo loại
+		//public ActionResult ProductType(string MaLoaiSP = 0)
+		//{
+		//   LOAISANPHAM sp = Db.LOAISANPHAMs.SingleOrDefault(n=>n.MALOAISP == MaLoaiSP)
+		//}
+		// -> Đang làm
+		public ActionResult SIZE(int id)
+		{
+			var ct = Db.CHITIETSANPHAMs.Where(n => n.MASP == id).ToList();
+			if (ct == null)
+			{
+				Response.StatusCode = 404;
+				return null;
+			}
+			// Truyền vào model là sản phẩm
+			return PartialView(ct);
+		}
+		public ActionResult extendProduct(int maloai,int masp)
+		{
+			var exProduct = Db.SANPHAMs.OrderBy(n => n.MALOAISP).Where(n=>n.MALOAISP==maloai  ).Where(n=>n.MASP!=masp).Take(3).ToList();
+			return PartialView(exProduct);
+		}
+	}
+}
---
+++ VienShops/VienShops/Models/AccountViewModels.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace VienShops.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required(ErrorMessage = "Bạn chưa nhập Email")]
+        [Display(Name = "Email")]
+		[StringLength(255, ErrorMessage = "Email dài tối đat 255 kí tự")]
+		public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Lưu trình duyệt ?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required(ErrorMessage = "Bạn chưa nhập Email")]
+        [Display(Name = "Email")]
+		[StringLength(255, ErrorMessage = "Email tối đa 255 kí tự ")]
+		public string Email { get; set; }
+    }
+
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage = "Bạn Chưa nhập Email")]
+		[Display(Name = "Email")]
+		[EmailAddress]
+		[StringLength(255, ErrorMessage = "Email tối đa 255 kí tự ")]
+		public string Email { get; set; }
+
+		[Required (ErrorMessage = "Bạn chưa nhập mật khẩu ")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Mật khẩu")]
+		public string Password { get; set; }
+
+		[Display(Name = "Remember me?")]
+		public bool RememberMe { get; set; }
+
+    }
+
+    public class RegisterViewModel
+    {
+		[Required(ErrorMessage = "Bạn chưa nhập Tên")]
+		[Display(Name = "Tên")]
+		[StringLength(255)]
+		public string FullName { get; set; }
+		[Required(ErrorMessage = "Bạn chưa nhập Email")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Mật Khẩu phải có tối đa {0} kí tự và tối thiểu {2} kí tự.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Xác thực mật khẩu")]
+        [Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng ")]
+        public string ConfirmPassword { get; set; }
+		[Required(ErrorMessage = "Bạn chưa nhập địa chỉ")]
+		[Display(Name = "Địa chỉ")]
+		[StringLength(255)]
+		public string Adress { get; set; }
+	}
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+		[StringLength(255, ErrorMessage = "Email không được bỏ trống")]
+		public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng .")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+		[StringLength(255, ErrorMessage = "Email không được bỏ trống")]
+		public string Email { get; set; }
+    }
+}
